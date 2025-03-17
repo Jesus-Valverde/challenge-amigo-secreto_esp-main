2fea6978dab112a88fd276630f93a008b7b0260b
Jesus Valverde <jesusvalverde.dev@outlook.com>
2025-03-17 16:27:39 -0700

    Agregada funcion AgregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
+
+function agregarAmigo() {
+    let campoNombre = document.getElementById('amigo');
+    if (campoNombre.value.trim() === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        nombres.push(campoNombre.value.trim());
+        console.log(nombres);
+    }
+    return;
+}
